022282f1c923b4fbf592e14f551da8e5bef3fc15
23120328 <buuhuynhvinhphuc@gmail.com>
2026-06-18 01:47:00 +0700

    remove uncertain sol

--- Source/main.py
+++ Source/main.py
@@ -10,12 +10,12 @@ def run_solver():
 
     # 2. Giả lập kết quả sau khi chạy qua thuật toán thông minh của bạn
     # (Ở đây lấy ví dụ ma trận giải mẫu 4x4 từ đề bài)
-    mock_solved_grid = [
-        [2, 3, 4, 1],
-        [1, 2, 3, 4],
-        [4, 1, 2, 3],
-        [3, 4, 1, 2]
-    ]
+    # mock_solved_grid = [
+    #     [2, 3, 4, 1],
+    #     [1, 2, 3, 4],
+    #     [4, 1, 2, 3],
+    #     [3, 4, 1, 2]
+    # ]
 
     # 3. Xuất kết quả tự động đan xen các dấu bất đẳng thức ra file text tương ứng
     puzzle.save_solution(mock_solved_grid, "Outputs/output-01.txt")
